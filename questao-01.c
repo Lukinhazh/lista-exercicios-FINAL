3483d104bd17ad0fb5485235df672c62244a7cd6
little lucas <132906568+Lukinhazh@users.noreply.github.com>
2023-06-11 17:23:10 -0300

    Create questao-01.c

---
+++ questao-01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float r, d, comp, area;
+
+    printf("Digite o raio da circunferência: ");
+    scanf("%f", &r);
+
+    d = 2 * r;
+
+    comp = 2 * 3.1415926535897932384626433832795 * r;
+
+    area = 3.1415926535897932384626433832795 * r * r;
+
+    printf("Diâmetro: %.2f\n", d);
+    printf("Comprimento: %.2f\n", comp);
+    printf("Área: %.2f\n", area);
+
+    return 0;
+}
